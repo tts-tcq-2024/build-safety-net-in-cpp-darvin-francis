1d6939a53e5ae074bd3a794c7c90606868df8514
darvin-francis <darvinfrancis30@gmail.com>
2024-09-06 11:30:30 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -1,27 +1,24 @@
-#include "Soundex.h"
 #include <cctype>
-using namespace std;
+#include <string>
 
 char getSoundexCode(char c) {
-    static const std::unordered_map<char, char> soundexCodes = {
-        {'B', '1'}, {'F', '1'}, {'P', '1'}, {'V', '1'},
-        {'C', '2'}, {'G', '2'}, {'J', '2'}, {'K', '2'}, {'Q', '2'}, {'S', '2'}, {'X', '2'}, {'Z', '2'},
-        {'D', '3'}, {'T', '3'},
-        {'L', '4'},
-        {'M', '5'}, {'N', '5'},
-        {'R', '6'}
+    static const std::string soundexCodes[] = {
+        "", "BFPV", "CGJKQSXZ", "DT", "L", "MN", "R"
     };
 
     c = toupper(c);
-    auto it = soundexCodes.find(c);
-    return (it != soundexCodes.end()) ? it->second : '0';
+    for (int i = 1; i < 7; i++) {
+        if (soundexCodes[i].find(c) != std::string::npos) {
+            return '0' + i;
+        }
+    }
+    return '0'; // For A, E, I, O, U, H, W, Y
 }
 
 std::string generateSoundex(const std::string& name) {
     if (name.empty()) return "";
 
-    std::string soundex;
-    soundex += toupper(name[0]);
+    std::string soundex(1, toupper(name[0]));
     char prevCode = getSoundexCode(name[0]);
 
     for (size_t i = 1; i < name.length() && soundex.length() < 4; ++i) {
@@ -32,7 +29,9 @@ std::string generateSoundex(const std::string& name) {
         }
     }
 
-    soundex.resize(4, '0'); // Ensure the length is exactly 4
+    while (soundex.length() < 4) {
+        soundex += '0';
+    }
 
     return soundex;
 }
