de5ce97a1a6424e332328adf60890a2e8f302da4
darvin-francis <darvinfrancis30@gmail.com>
2024-09-11 11:55:52 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -3,6 +3,12 @@
 
 #include <string>
 
+char getSoundexCode(char c);
+std::string initializeSoundex(const std::string& name);
+bool shouldAddCode(char code, char prevCode);
+void processCharacter(char code, std::string& soundex, char& prevCode);
+void updateSoundex(std::string& soundex, const std::string& name);
+void padSoundex(std::string& soundex);
 std::string generateSoundex(const std::string& name);
 
 #endif // SOUNDEX_H
